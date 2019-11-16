bdc45b2da399663d5b4f7eef40996257cff3d2d6
Lynkfox <lynkfox@gmail.com>
2019-11-16 08:57:15 -0500

    Initial Commit

---
+++ GDC2019Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GDC2019Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
