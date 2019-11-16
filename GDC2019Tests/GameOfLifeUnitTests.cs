913bcc22063ca91092e319911221786cb257db42
Lynkfox <lynkfox@gmail.com>
2019-11-16 08:59:53 -0500

    Name changes

--- GDC2019Tests/GameOfLifeUnitTests.cs
+++ GDC2019Tests/GameOfLifeUnitTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GDC2019Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class GameOfLifeUnitTests
     {
         [TestMethod]
         public void TestMethod1()
